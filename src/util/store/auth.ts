ff7175867a4a18d041d2a0ca7ff43c6332383f73
LucasKwak <lkwakiglesia@gmail.com>
2024-08-24 19:08:18 +0200

    [AUTHENTICATION] The role of the user is saved in the store (this value will be used to show (or not) some features to users depending on their role)

--- src/util/store/auth.ts
+++ src/util/store/auth.ts
@@ -1,11 +1,15 @@
 import { defineStore } from "pinia";
 import IUser from "@/util/interfaces/IUser";
+import router from '@/util/router';
 
 export const useAuthStore = defineStore(
     "auth",
     {
         state: () => ({
             isAuthenticated: false, // Definimos jwt como string o null
+            isAdmin: false,
+            isAssistant: false,
+            isCustomer: false,
             baseURL: "http://localhost:9191/my-backend",
         }),
         actions: {
@@ -36,7 +40,7 @@ export const useAuthStore = defineStore(
                     }
 
                     // Establecemos que el usuario esta autenticado
-                    this.isAuthenticated = true;
+                    await this.getRole();
                     return true;
                 } catch (error) {
                     console.error("Error en la solicitud:", error);
@@ -67,7 +71,8 @@ export const useAuthStore = defineStore(
                     }
 
                     // Establecemos que el usuario esta autenticado
-                    this.isAuthenticated = true;
+                    await this.getRole();
+
                     return true;
                 } catch (error) {
                     console.error("Error en la solicitud:", error);
@@ -98,6 +103,9 @@ export const useAuthStore = defineStore(
                     }
 
                     // Establecemos que el usuario ya no esta autenticado
+                    this.isCustomer = false;
+                    this.isAssistant = false;
+                    this.isAdmin = false;
                     this.isAuthenticated = false;
                     return true;
                 } catch (error) {
@@ -119,12 +127,19 @@ export const useAuthStore = defineStore(
                         },
                     });
 
-                    // Verificar el codigo de estado de la respuesta HTTP
+                    if (rawResponse.status == 401) {
+                        this.isAuthenticated = false;
+                        await router.push("/sign-in");
+                        return null;
+                    }
+
+                    // Verificar el codigo de estado de la respuesta HTTP (cualquier otro error)
                     if (rawResponse.status !== 200) {
                         const errorResponse = await rawResponse.json();
                         console.error(errorResponse.message);
                         return null;
                     }
+
                     const response = await rawResponse.json();
 
                     user = {
@@ -152,7 +167,15 @@ export const useAuthStore = defineStore(
                         },
                     });
 
-                    // Verificar el codigo de estado de la respuesta HTTP
+                    // En caso de que el token que haya en las cookies este caducadoo tenga una forma invalida
+                    if (rawResponse.status == 401) {
+                        console.log("ESTOY AQUI")
+                        this.isAuthenticated = false;
+                        await router.push("/sign-in");
+                        return;
+                    }
+
+                    // Verificar el codigo de estado de la respuesta HTTP (cualquier otro error)
                     if (rawResponse.status !== 200) {
                         this.isAuthenticated = false;
                         const errorResponse = await rawResponse.json();
@@ -162,13 +185,75 @@ export const useAuthStore = defineStore(
 
                     const value = await rawResponse.json();
 
+                    await this.getRole();
+
                     // Establecemos que el usuario esta autenticado
-                    this.isAuthenticated = value;
+                    //this.isAuthenticated = value;
                 } catch (error) {
                     console.error("Error en la solicitud:", error);
                     this.isAuthenticated = false;
                 }
 
+            },
+            async getRole(): Promise<void> {
+                const uri = `${this.baseURL}/auth/get-role`;
+                try {
+                    const rawResponse = await fetch(uri, {
+                        method: "GET",
+                        credentials: 'include',
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json",
+                        },
+                    });
+
+                    // En caso de que el token que haya en las cookies este caducadoo tenga una forma invalida
+                    if (rawResponse.status == 401) {
+                        console.log("ESTOY AQUI")
+                        this.isAuthenticated = false;
+                        await router.push("/sign-in");
+                        return;
+                    }
+
+                    // Verificar el codigo de estado de la respuesta HTTP (cualquier otro error)
+                    if (rawResponse.status !== 200) {
+                        this.isAuthenticated = false;
+                        const errorResponse = await rawResponse.json();
+                        console.error(errorResponse.message);
+                        return;
+                    }
+
+                    this.isCustomer = false;
+                    this.isAdmin = false;
+                    this.isAssistant = false;
+
+                    const response = await rawResponse.json();
+
+                    console.log(response.role);
+
+                    if(response.role == "CUSTOMER"){
+                        this.isCustomer = true;
+                    }
+
+                    if(response.role == "ASSISTANT_ADMINISTRATOR"){
+                        this.isAssistant = true;
+                    }
+
+                    if(response.role == "ADMINISTRATOR"){
+                        this.isAdmin = true;
+                    }
+
+                    // Establecemos que el usuario esta autenticado
+                    this.isAuthenticated = true;
+
+                    console.log(this.isCustomer);
+                    console.log(this.isAdmin);
+                    console.log(this.isAssistant);
+
+                } catch (error) {
+                    console.error("Error en la solicitud:", error);
+                    this.isAuthenticated = false;
+                }
             },
             setIsAuthenticated(yesOrNo:boolean): void {
                 this.isAuthenticated = yesOrNo;
